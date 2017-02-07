20443689746aead4b412d6aad89df6b42d028114
Nathan Goodman <natecbd@natecbd.com>
2017-02-06 18:34:35 -0800

    changes for 2/6/2017

--- NRCCorpWebsite/Index.js
+++ NRCCorpWebsite/Index.js
@@ -1,5 +1,11 @@
 $(document).ready(function () { 
     setTimeout(function() {
         $('#NRC-Banner').addClass('hover');
+        $('#NRC-Banner').hover(function() {
+            $('#NRC-Banner').removeClass('hover');
+        })
     }, 500);
+    setTimeout(function() {
+        $('#NRC-Banner').removeClass('hover');
+    }, 5500);
 });
